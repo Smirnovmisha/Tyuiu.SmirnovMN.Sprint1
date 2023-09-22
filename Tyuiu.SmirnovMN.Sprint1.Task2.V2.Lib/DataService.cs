970359d87af87506731a20d38d035748f2218589
Tyuiu.SmirnovMN@outlook.com <Tyuiu.SmirnovMN@outlook.com>
2023-09-22 22:52:40 +0500

    Task2

---
+++ Tyuiu.SmirnovMN.Sprint1.Task2.V2.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SmirnovMN.Sprint1.Task2.V2.Lib
+{
+    public class DataService : ISprint1Task2V2
+    {
+        public double ConvertAngleToRad(int value)
+        {
+            return value * (180 / Math.PI);
+        }
+    }
+}
---
+++ Tyuiu.SmirnovMN.Sprint1.Task2.V2.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SmirnovMN.Sprint1.Task2.V2.Lib;
+
+namespace Tyuiu.SmirnovMN.Sprint1.Task2.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            var res = ds.ConvertAngleToRad(x);
+            Assert.AreEqual(0, 360, res);
+        }
+    }
+}
